ede169635a3a1845bc8d319dd99c213466999568
Jignesh Patil <patiljignesh@gmail.com>
2021-07-20 13:33:55 +0200

    Add method requirements

---
+++ Protocol.playground/Pages/Protocol Method Requirements.xcplaygroundpage/Contents.swift
@@ -0,0 +1,18 @@
+//: [Previous](@previous)
+
+import Foundation
+
+protocol FullName {
+    
+    // Properties
+    var firstName: String {get set}
+    var lastName: String {get set}
+    
+    // Method Definition, block/Medthod body is handled
+    // by the Type which is consuming this Protocol
+    func getFullName() -> String
+    
+    // This method is allowed to modify the instance it belongs to
+    mutating func changeName() -> String
+    
+}
